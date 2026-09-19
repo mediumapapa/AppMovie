15f5cb0dcfcf198d5e02481b1a77a32e1164a3a3
Santiago Guerrero <guerrero.colin.santiago@gmail.com>
2026-09-19 17:54:48 -0600

    Inicializacion del proyecto

---
+++ movies_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movies_appApp.swift
+//  movies_app
+//
+//  Created by Alberto Ávila Aguilar on 18/09/26.
+//
+
+import SwiftUI
+
+@main
+struct movies_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
